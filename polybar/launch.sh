e4e80929a40cb25c1e09eaca1c15fcf7af8504e6
Aiizu-27 <luciiiia.2713@gmail.com>
2026-01-16 20:34:14 +0100

    barra

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Mata barras antiguas
+# 1. Matar las barras que ya estén corriendo para no duplicarlas
 killall -q polybar
 
-# Espera que se cierren
+# 2. Esperar un poquito a que se cierren del todo
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Lanza la barra
-polybar mybar &
+# 3. AQUÍ es donde la lanzas (esta es la línea importante)
+# La 'y' comercial (&) al final es vital para que no se congele la terminal
+polybar example &
+
+echo "Polybar lanzada..."
